e4ea0b3a85e39ca982f99911a41f2afd22b7179e
Donovan <mcdonovan@outlook.fr>
2019-10-21 18:56:27 -0500

    add of the next event home card; currently messing with the drink list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,25 +8,50 @@ import * as firebase from 'firebase';
 })
 export class HomePage {
 
-	ref = firebase.database().ref("beer_of_the_month");
+	public ref;
 	public botm = {
 		name: "name_1",
 		alcohol: "6",
 		type: "lager",
 		description: "description bla bla bla",
 		price_250: 0,
-		price_500: 0
+		price_500: 0,
+		img: "nothing bro"
+	}
+	public nextEve = {
+		name: "eve",
+		date: "02/11/2021",
+		description: "Enter even description here",
+		img: "nop"
 	}
 
   constructor() {
+		this.getBeerOfTheMonth();
+		this.getNextEvent();
+	}
+
+	getBeerOfTheMonth() {
+		this.ref = firebase.database().ref("beer_of_the_month");
 		// this.ref.push(this.botm); //create a new beer of the month into the database
 		this.ref.on("value", resp => {
-		resp.forEach(childSnapshot => {
-			let item = childSnapshot.val();
-			console.log(item);
-			this.botm = item;
-			console.log(this.botm);
+			resp.forEach(childSnapshot => {
+				let item = childSnapshot.val();
+				console.log(item);
+				this.botm = item;
+			});
 		});
-	});}
+	}
+
+	getNextEvent() {
+		this.ref = firebase.database().ref("next_event");
+		// this.ref.push(this.nextEve); //create a new beer of the month into the database
+		this.ref.on("value", resp => {
+			resp.forEach(childSnapshot => {
+				let item = childSnapshot.val();
+				console.log(item);
+				this.nextEve = item;
+			});
+		});
+	}
 
 }
--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -7,28 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
   private selectedItem: any;
-  private icons = [
-    'flask',
-    'wifi',
-    'beer',
-    'football',
-    'basketball',
-    'paper-plane',
-    'american-football',
-    'boat',
-    'bluetooth',
-    'build'
-  ];
+  // private icons = [
+  //   'flask',
+  //   'wifi',
+  //   'beer',
+  //   'football',
+  //   'basketball',
+  //   'paper-plane',
+  //   'american-football',
+  //   'boat',
+  //   'bluetooth',
+  //   'build'
+  // ];
+	private beers = []
+
   public items: Array<{ title: string; note: string; icon: string }> = [];
   constructor() {
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+    // for (let i = 1; i < 11; i++) {
+    //   this.items.push({
+    //     title: 'Item ' + i,
+    //     note: 'This is item #' + i,
+    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+    //   });
+    // }
   }
+
+
+
+
+
+
 
   ngOnInit() {
   }
