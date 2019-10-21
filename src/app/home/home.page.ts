2b41a3c2f16f9806551319e115faab5866c6c481
Donovan <mcdonovan@outlook.fr>
2019-10-21 17:55:32 -0500

    link to databse successfull for the beer of the month

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+	ref = firebase.database().ref("beer_of_the_month");
+	public botm = {
+		name: "name_1",
+		alcohol: "6",
+		type: "lager",
+		description: "description bla bla bla",
+		price_250: 0,
+		price_500: 0
+	}
+
+  constructor() {
+		// this.ref.push(this.botm); //create a new beer of the month into the database
+		this.ref.on("value", resp => {
+		resp.forEach(childSnapshot => {
+			let item = childSnapshot.val();
+			console.log(item);
+			this.botm = item;
+			console.log(this.botm);
+		});
+	});}
 
 }
