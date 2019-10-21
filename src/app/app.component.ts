1874c0aaac0479e9558c485c6fe0227bb2b45a7b
Donovan <mcdonovan@outlook.fr>
2019-10-21 14:16:46 -0500

    firebase initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -31,7 +32,17 @@ export class AppComponent {
     this.initializeApp();
   }
 
-  initializeApp() {
+	initializeApp() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyAWv7wd-GkRuj7lsnniYyLSVkkczKU8m5k",
+	    authDomain: "barapp-5a66d.firebaseapp.com",
+	    databaseURL: "https://barapp-5a66d.firebaseio.com",
+	    projectId: "barapp-5a66d",
+	    storageBucket: "barapp-5a66d.appspot.com",
+	    messagingSenderId: "245183107029",
+	    appId: "1:245183107029:web:95b9f06b3b697ac0aab75c",
+	    measurementId: "G-JB0MJ3Z6CH"
+    });
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
